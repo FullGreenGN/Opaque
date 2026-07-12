f4e85c03005abc57ea085bb7ecd2228e65d2b0a3
fullgreen.gn <j.luc@polarisdev.fr>
2026-07-12 17:04:06 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { expo } from "@better-auth/expo";
+import { createPrismaClient } from "@no-chat/db";
+import { env } from "@no-chat/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export function createAuth() {
+  const prisma = createPrismaClient();
+
+  return betterAuth({
+    database: prismaAdapter(prisma, {
+      provider: "postgresql",
+    }),
+
+    trustedOrigins: [env.CORS_ORIGIN, "no-chat://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
